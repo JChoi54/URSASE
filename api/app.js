7c75e3637ca6791b33e3f7149560c47b10b6bd67
KarmaPanda <desmondlai30@gmail.com>
2020-12-28 18:41:48 -0500

    Removed config.js from GitHub, added bcrypt package.

--- api/app.js
+++ api/app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 // Express Setup
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- api/db/config.js
+++
@@ -1,7 +0,0 @@
-module.exports.databaseOptions = {
-    host     : 'localhost',
-    database : 'ursase',
-    user     : 'ursase',
-    password : 'qwerty',
-    port     : '8889'
-};
