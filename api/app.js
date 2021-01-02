16974bf5dc185b54319baf5c0c969c3deec0392d
KarmaPanda <desmondlai30@gmail.com>
2021-01-01 22:46:59 -0500

    Finished basic sign in system on backend and laid out plan for sign up and verify email.

--- api/app.js
+++ api/app.js
@@ -5,8 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
-const indexRouter = require('./routes/index');
-const accountRouter = require('./routes/account');
+const mysql = require('mysql');
+const db = require('./db/db');
 
 const app = express();
 
@@ -17,29 +17,92 @@ app.set('view engine', 'ejs');
 // Express Setup
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes Setup
-app.use('/api', indexRouter);
-app.use('/api/account', accountRouter);
+// Body Parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// bcrypt
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.post('/api/signin', async function (req, res) {
+    let email = req.body.email
+    let password = req.body.password
 
-  // render the error page
-  res.status(err.status || 500);
+    // STEP 1: Find user by email in database.
+    try {
+        db.query("SELECT * FROM users WHERE emailaddress=?", [email], async function (err, results, fields) {
+            const user = results[0]
 
-  // TODO: Create a error page.
-  res.render('error');
-});
+            if (user !== undefined) {
+                // STEP 2: Compare passwords in database with hash.
+                const hash = await bcrypt.hash(password, saltRounds);
+
+                bcrypt.compare(user.password, hash, function (err, result) {
+                    if (err) throw err;
+
+                    if (result) {
+                        // STEP 3: Log the user in.
+                        res.send("Password is correct.")
+                    } else {
+                        res.send("Password is incorrect.")
+                    }
+                });
+            }
+            else {
+                res.send("User does not exist.")
+            }
+        })
+    } catch (e) {
+        console.error("/api/signin: " + e.message)
+    }
+})
+
+app.post('/api/signout', function (req, res) {
+    console.log(req.body);
+    res.send("")
+
+    // STEP 1: Check if user was already signed on through session cookie.
+
+    // STEP 2:
+})
+
+app.post('/api/signup', function (req, res) {
+    console.log(req.body);
+
+    let email = req.body.email
+    let password = req.body.password
+
+    // STEP 1: Check if user already exists on the database.
+
+    // STEP 2: If user does not already exists, hash password in bcrypt.
+
+    // TODO: Use the user model defined under db/models
+
+    // STEP 3: Once hash is completed, create the user model and store the new user into the database.
+
+    // TODO: Add email verification step. Generate email verification token and add it to Step 3.
+
+    // STEP 4: Send response back to react letting them know to verify their email.
+
+    // STEP 4b: Send email to user with verification email letting them know they need to verify.
+})
+
+app.post('/api/verifyemail', function (req, res) {
+    console.log(req.body);
+
+    // STEP 1: Check if the email verification token exists.
+
+    // STEP 2: If verification token exists, nullify the verification token, and set email verification variable to true in database.
+
+    // STEP 3: Send response to react letting them know they verified their email successfully, and redirect to login page.
+
+})
 
 module.exports = app;
--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './SignInSignUp.css';
 import '../App.css';
 
-function SignIn(){
-    return(
-        <form action="" className="signin-form">
-            <h2>Log In</h2>
-            <div className="input-field">
-                <i className="fas fa-envelope"></i>
-                <input type="text" placeholder="Email"></input>
-            </div>
-            <div className="input-field">
-                <i className="fas fa-lock"></i>
-                <input type="password" placeholder="Password"></input>
-            </div>
-            <input type="submit" value="Login" className="click-button"></input>
-        </form>
-    )
+class SignIn extends Component {
+    state = {
+        email: '',
+        password: '',
+        postResponse: '',
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        const response = await fetch('/api/signin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email: this.state.email, password: this.state.password})
+        });
+        const body = await response.text();
+        this.setState({postResponse: body})
+    }
+
+    render() {
+        return (
+            <form action="#" className="signin-form" onSubmit={this.handleSubmit}>
+                <h2>Log In</h2>
+                <div className="input-field">
+                    <i className="fas fa-envelope"/>
+                    <input type="text" placeholder="Email" value={this.state.email} onChange={e => this.setState({ email: e.target.value })}/>
+                </div>
+                <div className="input-field">
+                    <i className="fas fa-lock"/>
+                    <input type="password" placeholder="Password" value={this.state.password} onChange={e => this.setState({ password: e.target.value })}/>
+                </div>
+                <input type="submit" value="Login" className="click-button"/>
+                <p>{this.state.postResponse}</p>
+            </form>
+        );
+    }
 }
 
 export default SignIn
