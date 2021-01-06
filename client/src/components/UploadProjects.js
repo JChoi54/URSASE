94c494c401005059059a7dc26463bb189dbf9848
Joshua <hijoshuachoi@gmail.com>
2021-01-06 03:26:45 -0500

    change upload project to text input for link

--- client/src/components/UploadProjects.js
+++ client/src/components/UploadProjects.js
@@ -5,27 +5,6 @@ import AccountSidebar from './AccountSidebar';
 
 function UploadProjects(){
 
-    const addProject = () => document.getElementById("project-file").click();
-    const addFeatured = () => document.getElementById("featured-file").click();
-
-    const projectText = () => {
-        if (document.getElementById("project-file").value) {
-            // eslint-disable-next-line
-            document.getElementById("custom-project-text").innerHTML = document.getElementById("project-file").value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
-        } else {
-            document.getElementById("custom-project-text").innerHTML = "No file chosen."
-        }
-    }
-
-    const featuredText = () => {
-        if (document.getElementById("featured-file").value) {
-            // eslint-disable-next-line
-            document.getElementById("custom-featured-text").innerHTML = document.getElementById("featured-file").value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
-        } else {
-            document.getElementById("custom-featured-text").innerHTML = "No file chosen."
-        }
-    }
-
     return(
         <div className="account-container">
             <div className="account">
@@ -36,16 +15,19 @@ function UploadProjects(){
                 <div className="account-setting">
                     <div className="upload-featured">
                         <h1>Featured Project</h1>
-                        <input type="file" id="featured-file" onChange={featuredText} hidden = "hidden"/>
-                        <button type="button" className="upload-button" onClick={addFeatured}>CHOOSE A FILE</button>
-                        <span id="custom-featured-text">No file chosen.</span>
+                        <div className="input-area">
+                            <label for="featuredlink">Link:</label>
+                            <input name="featuredlink" type="text" className="form-control" id="featuredlink"/>
+                        </div>
+                        
                     </div>
                         
                     <div className="upload-projects">
                         <h1>Upload Projects</h1>
-                        <input type="file" id="project-file" onChange={projectText} hidden = "hidden"/>
-                        <button type="button" className="upload-button" onClick={addProject}>CHOOSE A FILE</button>
-                        <span id="custom-project-text">No file chosen.</span>
+                        <div className="input-area">
+                            <label for="projectlink">Link:</label>
+                            <input name="projectlink" type="text" className="form-control" id="projectlink"/>
+                        </div>
                     </div>
                 </div>
             </div>
